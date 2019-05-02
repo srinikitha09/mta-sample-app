fc94c00426acf45746b49fe75ee65c7f6ad58df6
Florian Wilhelm <florian.wilhelm02@sap.com>
2019-05-02 14:36:26 +0200

    Update

--- srv/integration-tests/src/test/java/com/sap/cloud/sdk/examples/IntegrationTest.java
+++ srv/integration-tests/src/test/java/com/sap/cloud/sdk/examples/IntegrationTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.*;
 public class IntegrationTest {
     @Test
     public void test() {
+        HelloWorld.main(new String[]{"Hello", "World"});
         assertTrue(true);
     }
 }
